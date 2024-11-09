384087d4bd2f2dd4511038097ee41e3b98854def
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-11-09 01:33:58 -0500

    Update gameemulator.js

--- gameemulator.js
+++ gameemulator.js
@@ -1,19 +1,36 @@
 var EMULATOR = (function() {
 	class CPU {
+		constructor() {
+			this.memory = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // Make sure 256 digits of memory can be used
+		}
 		exec(instructions = new Uint8Array([0]), noHalt = false) {
 			/* Error Codes:
    				0: While not an error code, the program ran successfully
-       				1: An unknown accumulator was referenced/requested
+				1: An unknown accumulator was referenced/requested
 	   			2: An unknown instruction was encountered
+				3: An out-of-bounds instruction was encountered
    			*/
+			/*
+   				0: The program completely ends (no additional steps)
+				1: The program selects which accumulator would be requested for their value to be set
+				2: The program sets the selected accumulator's value to either an 8-bit integer (next instruction 0), 16-bit integer (next instruction 1), or an accumulator value (next instruction 2)
+			*/
+			if (this.memory.length !== 256 || !(this.memory instanceof Uint16Array)) throw new TypeError("The 'memory' property does not consist of 256 unsigned 16-bit integers! It has to only include 16-bit unsigned integers for the instructions to be fully executed.");
 			let i = 0, run = true;
-			let accumulators = new Uint16Array([0, 0, 0, 0]), accumulatorTarget = 0, accumulatorNames = ["A", "B", "C", "D"], status = 0;
+			let accumulators = new Uint16Array([0, 0, 0, 0]), accumulatorTarget = 0, accumulatorNames = "ABCD", status = 0;
 			function debug() {
 				console.warn("The error was found at instruction " + i);
 			}
 			function unknownAccumulator() {
 				if (noHalt) {
-					console.warn("/!\\ A non-existent accumulator was referenced for setting Accumulator " + accumulatorNames[accumulatorTarget] + ", so the default accumulator (Accumulator A) will be referenced.");
+					console.warn("/!\\ A non-existent accumulator was referenced for setting Accumulator " + accumulatorNames[accumulatorTarget] + "; the default accumulator (Accumulator A) will be referenced.");
 					return 0;
 				}
 				console.warn("(x) SyntaxError: A non-existent accumulator was referenced for setting Accumulator " + accumulatorNames[accumulatorTarget]);
@@ -28,10 +45,10 @@ var EMULATOR = (function() {
 						accumulatorTarget = instructions[i];
 						if (accumulatorTarget > 3) {
 							if (noHalt) {
-								console.warn("/!\\ A non-existent accumulator was requested for being set, so the default accumulator (Accumulator A) will be requested.");
+								console.warn("/!\\ A non-existent accumulator was requested for being set; the default accumulator (Accumulator A) will be requested");
 								accumulatorTarget = 0;
 							} else {
-								console.warn("(x) SyntaxError: A non-existent accumulator was requested for being set.");
+								console.warn("(x) SyntaxError: A non-existent accumulator was requested for being set");
 								debug();
 								run = false;
 								return 1;
@@ -53,19 +70,39 @@ var EMULATOR = (function() {
 							case 2:
 								i++;
 								accumulators[accumulatorTarget] = accumulators[instructions[i < 4 ? i : unknownAccumulator()]];
-								break
+								break;
+						}
+						break;
+					case 3:
+						i++;
+						switch (instructions[i]) {
+							case 0:
+								i++;
+								accumulators[accumulatorTarget] += instructions[i];
+								break;
+							case 1:
+								i++;
+								accumulators[accumulatorTarget] += instructions[i] + (instructions[i + 1] << 8);
+								i++;
+								break;
+							case 2:
+								i++;
+								accumulators[accumulatorTarget] += accumulators[instructions[i < 4 ? i : unknownAccumulator()]];
+								break;
 						}
 						break;
 					case undefined:
-						console.warn("/!\\ An instruction was encountered out-of-bounds, so the program has been halted to prevent serious consequences.");
+						console.warn("(x) An instruction was encountered out-of-bounds, so the program has been halted to prevent serious consequences");
+						run = false;
+						return 3;
 					case 0:
 						run = false;
 						break;
 					default:
 						if (noHalt) {
-							console.warn("/!\\ An unknown instruction was encountered (instruction is " + instructions[i] + "), but that instruction will be ignored.");
+							console.warn("/!\\ An unknown instruction was encountered (instruction is " + instructions[i] + "), but that instruction will be ignored");
 						} else {
-							console.warn("(x) SyntaxError: An unknown instruction was encountered (instruction is " + instructions[i] + ").");
+							console.warn("(x) SyntaxError: An unknown instruction was encountered (instruction is " + instructions[i] + ")");
 							run = false;
 							return 2;
 						}
