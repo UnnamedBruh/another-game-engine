50a1919ec22e657d41bbc1127885556784417fcf
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-11-10 12:06:13 -0500

    Update gameemulator.js

--- gameemulator.js
+++ gameemulator.js
@@ -1,4 +1,4 @@
-var EMULATOR = (function() {
+var EMULATOR = (function(canvas) {
 	const children = [];
 	const CPUDefaultMemory = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -32,7 +32,8 @@ var EMULATOR = (function() {
 			*/
 			if (this.memory.length !== 256 || !(this.memory instanceof Uint16Array)) throw new TypeError("The 'memory' property does not consist of 256 unsigned 16-bit integers! It has to only include 16-bit unsigned integers for the instructions to be fully executed.");
 			let i = 0, run = true, memClear = true;
-			let accumulators = new Uint16Array([0, 0, 0, 0]), accumulatorTarget = 0, accumulatorNames = "ABCD", status = 0;
+			let accumulators = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0]), accumulatorTarget = 0, accumulatorNames = "ABCDEFGH", status = 0;
+			let accStrings = ["", "", "", "", "", "", "", ""];
 			function debug() {
 				console.warn("The error was found at instruction " + i);
 			}
@@ -51,7 +52,7 @@ var EMULATOR = (function() {
 					case 1:
 						i++;
 						accumulatorTarget = instructions[i];
-						if (accumulatorTarget > 3) {
+						if (accumulatorTarget > 7) {
 							if (noHalt) {
 								console.warn("/!\\ A non-existent accumulator was requested for being set; the default accumulator (Accumulator A) will be requested");
 								accumulatorTarget = 0;
@@ -77,11 +78,27 @@ var EMULATOR = (function() {
 								break;
 							case 2:
 								i++;
-								accumulators[accumulatorTarget] = accumulators[instructions[i < 4 ? i : unknownAccumulator()]];
+								accumulators[accumulatorTarget] = accumulators[instructions[i < 8 ? i : unknownAccumulator()]];
 								break;
 							case 3:
 								i++;
 								accumulators[accumulatorTarget] = this.memory[instructions[i]];
+								break;
+							case 4:
+								i++;
+								accStrings[accumulatorTarget] = "";
+								while (instructions[i] !== 0) {
+									accStrings[accumulatorTarget] += String.fromCodePoint(instructions[i]);
+									i++;
+								}
+								break;
+							case 5:
+								i++;
+								accStrings[accumulatorTarget] = "";
+								while (instructions[i] + instructions[i + 1] !== 0) {
+									accStrings[accumulatorTarget] += String.fromCodePoint(instructions[i] + (256 * instructions[i + 1]));
+									i += 2;
+								}
 								break;
 						}
 						break;
@@ -99,11 +116,25 @@ var EMULATOR = (function() {
 								break;
 							case 2:
 								i++;
-								accumulators[accumulatorTarget] += accumulators[instructions[i < 4 ? i : unknownAccumulator()]];
+								accumulators[accumulatorTarget] += accumulators[instructions[i < 8 ? i : unknownAccumulator()]];
 								break;
 							case 3:
 								i++;
 								accumulators[accumulatorTarget] += this.memory[instructions[i]];
+								break;
+							case 4:
+								i++;
+								while (instructions[i] !== 0) {
+									accStrings[accumulatorTarget] += String.fromCodePoint(instructions[i]);
+									i++;
+								}
+								break;
+							case 5:
+								i++;
+								while (instructions[i] + instructions[i + 1] !== 0) {
+									accStrings[accumulatorTarget] += String.fromCodePoint(instructions[i] + (256 * instructions[i + 1]));
+									i += 2;
+								}
 								break;
 						}
 						break;
@@ -130,7 +161,7 @@ var EMULATOR = (function() {
 								break;
 							case 2:
 								i++;
-								this.memory[memorySelect] = accumulators[instructions[i < 4 ? i : unknownAccumulator()]];
+								this.memory[memorySelect] = accumulators[instructions[i < 8 ? i : unknownAccumulator()]];
 								break;
 							case 3:
 								i++;
@@ -159,6 +190,89 @@ var EMULATOR = (function() {
 			}
 			return status;
 		}
+		compile(code) {
+			if (typeof code !== "string") return;
+			const regex = /\[[A-Z0-9]+\]|\<[A-Z_]+\>|"[^"]*"|\w+|-?\d+(\.\d*)?|[\n;](?:[\n;]*)|[^ \t]/gms;
+			const tokens = code.match(regex);
+			const len = tokens.length, arr = [], types = {UINT8: 0, UINT16: 1, UTF8STRING: 4, UTF16STRING: 5};
+			let i = 0, currentAccumulator = 0, accumulators = 0, token;
+			let vars = {}, existingVars = [];
+			function handleType(type, literal) {
+				const result = [];
+				let num = 0;
+				switch (type) {
+					case "UINT8":
+						result.push(Math.floor(+literal) % 256);
+						break;
+					case "UINT16":
+						num = +literal;
+						result.push(Math.floor(num) % 256, (num << 8) % 256);
+						break;
+					case "UTF8STRING":
+						num = literal.slice(1, -1);
+						const len = num.length;
+						let char = 0;
+						for (let i = 0; i < len; i++) {
+							char = num.codePointAt(i);
+							if (char > 255) throw new TypeError("COMPILATION:\n(x) [UTF8STRING]s must have characters with their code points less than 255!");
+							if (char === 0) {
+								result.push(char);
+								console.warn("COMPILATION:\n/!\\ It is not recommended to include any nullish characters in any [STRING] type! Doing so can lead to security vulnerabilities, and confusion with string literals!");
+								return result;
+							}
+							result.push(char);
+						}
+						result.push(0);
+						break;
+					case "UTF16STRING":
+						num = literal.slice(1, -1);
+						const len = num.length;
+						let char = 0;
+						for (let i = 0; i < len; i++) {
+							char = num.codePointAt(i);
+							if (char === 0) {
+								result.push(char, char);
+								console.warn("COMPILATION:\n/!\\ It is not recommended to include any nullish characters in any [STRING] type! Doing so can lead to security vulnerabilities, and confusion with string literals!");
+								return result;
+							}
+							result.push(char % 256, char << 8);
+						}
+						result.push(0, 0);
+						break;
+				}
+				return result;
+			}
+			function newIdentifier(id) {
+				if (existingVars.includes(id)) {
+					return vars[id];
+				}
+				vars[id] = accumulators;
+				existingVars.push(id);
+				return accumulators++;
+			}
+			for (; i < len; i++) {
+				token = tokens[i];
+				switch (token) {
+					case "SET":
+						i++;
+						if (/[^A-Za-z_]/.test(tokens[i])) {
+							throw new SyntaxError("COMPILATION:\n(x) Unexpected token '" + tokens[i] + "'");
+						}
+						let old = currentAccumulator;
+						currentAccumulator = newIdentifier(tokens[i]);
+						if (accumulators > 7) throw new Error("COMPILATION:\n(x) Only 8 variables can be defined. The compiler may handle this case in the future");
+						if (old !== currentAccumulator) {
+							arr.push(1, currentAccumulator);
+						}
+						i++;
+						old = tokens[i].slice(1, -1);
+						i++;
+						arr.push(types[old], ...handleType(old, tokens[i]));
+				}
+			}
+			arr.push(0);
+			return Uint8Array.from(arr);
+		}
 	}
-	
+	return CPU;
 })();
